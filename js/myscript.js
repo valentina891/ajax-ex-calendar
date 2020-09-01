fc8e492b6b1a56f66e65c14d817e57f3b3ef3052
vale <valemil.vs@gmail.com>
2020-09-01 14:30:23 +0200

    Milestone 2

--- js/myscript.js
+++ js/myscript.js
@@ -15,6 +15,9 @@
 
 $(document).ready(function(){
 
+    //leggo attributo per cambio mese:
+    var dataPartenza = moment($('h1.nome-mese').attr('data-formato'));
+
     // //creo oggetto moment con data di partenza: 2018-01-01
     var data = moment('2018-01-01');
 
@@ -22,68 +25,99 @@ $(document).ready(function(){
     insertDays(data);
     insertHolidays(data);
 
-    //definisco var per i gg, mesi e anno + uso daysInMonth per ottenere il n. dei gg con ciclo for:
+    $('button#successivo').click(function(){
+        next(dataPartenza);
+    });
 
-
-    // creo funzione per ins. giorni e festività:
-    function insertDays(data) {
-        var giorni = data.daysInMonth();
-        var mese = data.format('MMMM');
-        var anno = data.format('YYYY');
-        $('h1.nome-mese').html(mese + ' ' + anno);
-
-        for (var i = 1; i <= giorni; i++) {
-            var source = $("#template-giorni").html();
-            var template = Handlebars.compile(source);
-
-            var context = {
-                day: addZero(i),
-                month: mese,
-                year: anno,
-                dataCompleta: anno + '-' + data.format('MM') + '-' + addZero(i)
-            };
-            var html = template(context);
-
-            $('.giorni-mese').append(html);
-        }
-    }
-
-    function insertHolidays(data){
-        $.ajax({
-            url: 'https://flynn.boolean.careers/exercises/api/holidays',
-            method: 'GET',
-            data: {
-                year: data.year(),
-                month: data.month()
-            },
-            success: function(risposta){
-                var vacanze = risposta.response
-                // console.log(risposta.response);
-                for (var i = 0; i < vacanze.length; i++) {
-                    var vacanza = vacanze[i]
-                    console.log(vacanza);
-
-                    var lista = $('li[data-data-completa=' + vacanza.date +']');
-                    console.log(lista);
-                    lista.append(' - ' + vacanza.name)
-                    lista.addClass('holiday')
-                }
-            },
-            error: function(){
-                alert('errore')
-            }
-        })
-    }
-
-
-
+    $('button#precedente').click(function(){
+        prev(dataPartenza);
+    });
 });
 
-//****FUNZIONI:
+
+
+//****FUNZIONI****//:
 
 function addZero(n){
     if (n < 10) {
         return '0' + n
     }
     return n;
+}
+
+// creo funzione per ins. giorni e festività e definisco var per i gg, mesi e anno + uso daysInMonth per ottenere il n. dei gg con ciclo for:
+
+function insertDays(data) {
+
+    $('ul.giorni-mese').empty();
+
+    var giorni = data.daysInMonth();
+    var mese = data.format('MMMM');
+    var anno = data.format('YYYY');
+    $('h1.nome-mese').html(mese + ' ' + anno);
+
+    for (var i = 1; i <= giorni; i++) {
+        var source = $("#template-giorni").html();
+        var template = Handlebars.compile(source);
+
+        var context = {
+            day: addZero(i),
+            month: mese,
+            year: anno,
+            dataCompleta: anno + '-' + data.format('MM') + '-' + addZero(i)
+        };
+        var html = template(context);
+
+        $('.giorni-mese').append(html);
+    }
+}
+
+function insertHolidays(data){
+    $.ajax({
+        url: 'https://flynn.boolean.careers/exercises/api/holidays',
+        method: 'GET',
+        data: {
+            year: data.year(),
+            month: data.month()
+        },
+        success: function(risposta){
+            var vacanze = risposta.response
+            // console.log(risposta.response);
+            for (var i = 0; i < vacanze.length; i++) {
+                // var vacanze = risposta.response;
+                var vacanza = vacanze[i];
+                // console.log(vacanza);
+                var lista = $('li[data-data-completa="' + vacanza.date +'"]');
+                // console.log(lista);
+                lista.append(' - ' + vacanza.name);
+                lista.addClass('holiday');
+                console.log(lista);
+            }
+        },
+        error: function(){
+            alert('errore');
+        }
+    })
+}
+
+function next(data){
+
+    if (data.month() == 11) {
+        alert('Non puoi proseguire');
+    } else {
+        data.add(1,'months');
+        insertDays(data);
+        insertHolidays(data);
+    }
+}
+
+function prev(data){
+
+    if (data.month() == 0) {
+        alert('Non puoi proseguire');
+    } else {
+        data.subtract(1,'months');
+        insertDays(data);
+        insertHolidays(data);
+    }
 }
